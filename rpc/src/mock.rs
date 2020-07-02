b87bb2e00bdffb6a916e46d899595383ce86adc0
TopDev113 <topdev367@gmail.com>
2020-07-02 14:32:35 +0200

    fix mock

--- rpc/src/mock.rs
+++ rpc/src/mock.rs
@@ -1,87 +1,81 @@
-// Copyright 2017-2020 Parity Technologies (UK) Ltd.
-// This file is part of Frontier.
+// This file is part of Substrate.
 
-// Substrate is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Copyright (C) 2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
 
-// Substrate is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
-// GNU General Public License for more details.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+//! Mock file
 
-//! Test utilities
-
-use super::*;
-use pallet_ethereum::{Module, Trait};
-use ethereum::{Transaction, TransactionAction, TransactionSignature};
-use support::{parameter_types, weights::Weight};
-use pallet_evm::{FeeCalculator, HashTruncateConvertAccountId};
-use rlp::*;
-use sp_core::{H160, H256, U256};
+//use super::*;
+use support::{
+	parameter_types,
+	weights::{Weight, constants::WEIGHT_PER_SECOND},
+};
 use sp_runtime::{
+	traits::{IdentityLookup, Block as BlockT},
 	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup, IdentifyAccount, Verify},
-	ModuleId, Perbill, MultiSignature
 };
 
-pub type AccountId = <<MultiSignature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+type AccountId = u64;
+type AccountIndex = u32;
+type BlockNumber = u64;
+type Balance = u64;
 
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const MaximumBlockWeight: Weight = 1024;
-	pub const MaximumBlockLength: u32 = 2 * 1024;
-	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	pub const MaximumBlockWeight: Weight = 2 * WEIGHT_PER_SECOND;
 }
+
 impl system::Trait for Test {
 	type BaseCallFilter = ();
 	type Origin = Origin;
-	type Call = ();
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = H160;
+	type Index = AccountIndex;
+	type BlockNumber = BlockNumber;
+	type Call = Call;
+	type Hash = sp_core::H256;
+	type Hashing = ::sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
 	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = ();
-	type BlockHashCount = BlockHashCount;
+	type Header = sp_runtime::testing::Header;
+	type Event = Event;
+	type BlockHashCount = ();
 	type MaximumBlockWeight = MaximumBlockWeight;
 	type DbWeight = ();
-	type BlockExecutionWeight = ();
-	type ExtrinsicBaseWeight = ();
-	type MaximumExtrinsicWeight = MaximumBlockWeight;
-	type MaximumBlockLength = MaximumBlockLength;
-	type AvailableBlockRatio = AvailableBlockRatio;
+	type AvailableBlockRatio = ();
+	type MaximumBlockLength = ();
 	type Version = ();
 	type ModuleToIndex = ();
 	type AccountData = pallet_balances::AccountData<u64>;
 	type OnNewAccount = ();
-	type OnKilledAccount = ();
+	type OnKilledAccount = (Balances,);
+	type BlockExecutionWeight = ();
+	type ExtrinsicBaseWeight = ();
+	type MaximumExtrinsicWeight = ();
 }
-
 parameter_types! {
-	pub const ExistentialDeposit: u64 = 500;
+	pub const ExistentialDeposit: Balance = 10;
 }
-
 impl pallet_balances::Trait for Test {
-	type Balance = u64;
-	type Event = ();
+	type Balance = Balance;
+	type Event = Event;
 	type DustRemoval = ();
 	type ExistentialDeposit = ExistentialDeposit;
 	type AccountStore = System;
 }
 
-impl Trait for Test {
-	type Event = ();
-}
-
-pub type Block = sp_runtime::generic::Block<sp_runtime::testing::Header, UncheckedExtrinsic>;
-pub type UncheckedExtrinsic = sp_runtime::generic::UncheckedExtrinsic<u32, (), u64, ()>;
+pub type Block = sp_runtime::generic::Block<Header, UncheckedExtrinsic>;
+pub type UncheckedExtrinsic = sp_runtime::generic::UncheckedExtrinsic<u32, Call, u64, ()>;
 
 support::construct_runtime!(
 	pub enum Test where
@@ -90,16 +84,11 @@ support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic
 	{
 		System: system::{Module, Call, Event<T>},
-		Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>}
+		Balances: pallet_balances::{Module, Call, Storage, Config<T>, Event<T>},
 	}
 );
 
-
-pub fn new_test_ext(accounts_len: usize) -> sp_io::TestExternalities {
-	let ext = system::GenesisConfig::default()
-		.build_storage::<Test>()
-		.unwrap()
-		.into();
-
-	ext
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	let t = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	sp_io::TestExternalities::new(t)
 }
